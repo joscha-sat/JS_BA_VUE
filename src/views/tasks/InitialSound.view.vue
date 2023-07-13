6e7f07eec0176946d2755477fc3476ede08fdf6e
joscha-sat <joscha.sattler@web.de>
2023-07-13 23:55:12 +0200

    dialog tutorial: initial sound, phonemes

--- src/views/tasks/InitialSound.view.vue
+++ src/views/tasks/InitialSound.view.vue
@@ -1,12 +1,19 @@
 <!-- TS ------------------------------------------------------------//-->
 <script lang="ts" setup>
 
+const tutorialText = ref('At the top of your screen you see a letter. Below you see five image cards representing a word. You can listen to the word by clicking on the image or on the volume button. Drag all image cards where its word starts with the given letter at the top')
+
 </script>
 
 <!-- HTML ----------------------------------------------------------//-->
 <template>
+
+    <HelpMeBtn/>
+
     <v-container>
         <InitialSoundTask/>
+
+        <TutorialDialog :txt="tutorialText"/>
     </v-container>
 </template>
 
--- src/views/tasks/Phonemes.view.vue
+++ src/views/tasks/Phonemes.view.vue
@@ -1,11 +1,18 @@
 <!-- TS ------------------------------------------------------------//-->
 <script lang="ts" setup>
+
+const tutorialText = ref('On your screen you see an image card representing a word. You can listen to the word by clicking on the image or the volume button. Below you see four words and an empty drop are. Only one word is the correct spelling to the wanted word. Drag and drop the word with the correct spelling into the drop area. You can change the word with the arrow keys')
+
 </script>
 
 <!-- HTML ----------------------------------------------------------//-->
 <template>
+
+    <HelpMeBtn/>
     <v-container>
         <PhonemesTask/>
+
+        <TutorialDialog :txt="tutorialText"/>
     </v-container>
 </template>
 
